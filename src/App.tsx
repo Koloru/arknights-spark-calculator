84fe0e29f9eb51c967c9565f412ca10353a96c23
Koloru <sos.llyr@gmail.com>
2023-06-16 04:01:12 +0800

    create card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,65 @@
+import { useReducer } from "react";
+import Card from "./components/Card";
 
-function App() {
+import ticket from "./assets/Headhunting_Permit.webp";
+import originium from "./assets/Originite_Prime_icon.webp";
+import orundum from "./assets/Orundum_icon.webp";
+import bg from "./assets/young-handsome-man-with-beard-isolated-keeping-arms-crossed-frontal-position_1368-132662.jpeg";
 
-  return (
-    <h1 className='font-bold text-2xl underline'>
-      hello
-    </h1>
-  )
+const ReducerActionsTypes = {
+  ORUNDUM: "Orundum",
+  ORIGINIUM: "Originium",
+  TICKETS: "Tickets",
+};
+
+type ReducerActionsTypes = "Orundum" | "Originium" | "Tickets";
+
+interface ReducerActions {
+  type: ReducerActionsTypes;
+  payload: { orundum: number; tickets: number; originium: number };
 }
 
-export default App
+function reducer(
+  state: { orundum: number; tickets: number; originium: number },
+  action: ReducerActions
+) {
+  const { type, payload } = action;
+  
+  switch (type) {
+    case ReducerActionsTypes.ORIGINIUM:
+    case ReducerActionsTypes.ORUNDUM:
+    case ReducerActionsTypes.TICKETS:
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    orundum: 0,
+    tickets: 0,
+    originium: 0,
+  });
+
+  return (
+    <div
+      style={{
+        backgroundImage: `linear-gradient(101deg, rgba(0,0,0,0.6867121848739496) 0%, rgba(0,0,0,0.5466561624649859) 100%), url(${bg})`,
+      }}
+      className="flex justify-center w-full h-full p-4 overflow-x-hidden bg-center bg-cover font-lato"
+    >
+      <div className="flex flex-col gap-8 max-w-[900px] border border-white rounded-md p-4 w-full">
+        <div className="text-2xl font-bold text-center text-white">
+          Arknights Spark Calculator
+        </div>
+        <div className="flex flex-wrap justify-center gap-8">
+          <Card img={orundum} category="Orundum" />
+          <Card img={originium} category="Originium" />
+          <Card img={ticket} category="Headhunting Tickets" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,32 @@
+
+const Card = ({
+  img,
+  category,
+  state,
+  setState
+}: {
+  img: string;
+  state?: string;
+  setState: (e: string) => void
+  category: string;
+}) => {
+  return (
+    <div className={`relative flex flex-col items-center max-w-[200px]`}>
+      <div className="absolute flex flex-col items-center justify-center -top-6">
+        <div className="max-h-[45px] max-w-[50px]">
+          <img src={img} alt="" className="w-full h-full" />
+        </div>
+      </div>
+      <input
+        type="number"
+        className="px-2 py-2 max-w-[225px] text-xl font-bold text-center  outline-none "
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+      />
+      <div className="w-full font-black tracking-wide text-center text-white border-gray-700 rounded-b-md bg-sky-500">
+        {category}
+      </div>
+    </div>
+  );
+};
+export default Card;
